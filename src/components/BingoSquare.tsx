b9b8fc2ae37ccdd1d8ea0a6ec2823fb6380848ed
timshaww <time4shaw@gmail.com>
2024-02-29 13:01:50 -0500

    fixing build issues

--- src/components/BingoSquare.tsx
+++ src/components/BingoSquare.tsx
@@ -1,4 +1,4 @@
-function BingoSquare({ value, id, clicked, onClick }: { id: any, value: any, clicked: any, onClick: any }) {
+function BingoSquare({ value, clicked, onClick }: { id: any, value: any, clicked: any, onClick: any }) {
     
     return (
         <button className={`font-mono text-center overflow-clip text-xs rounded-lg hover:border-indigo-400 p-2 ${clicked ? 'bg-indigo-200' : 'bg-zinc-50'}`} onClick={onClick}>
