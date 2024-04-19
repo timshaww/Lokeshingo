dbb73cbc31fa9b9e03335210143a262142cafec6
timshaww <time4shaw@gmail.com>
2024-04-19 13:07:37 -0400

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lokeshingo',
+  base: '/Lokeshingo',
   plugins: [
     react()
   ],
